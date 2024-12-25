72bd8aeab50b01d8a67f914927188291ef273c0d
0nepeop1e <engshun.lim@gmail.com>
2024-12-25 15:37:49 +0800

    prerender privacy page

--- src/routes/privacy/+page.ts
+++ src/routes/privacy/+page.ts
@@ -1,6 +1,8 @@
 import { t } from '$lib/lang';
 import { derived } from 'svelte/store';
 
+export const prerender = true;
+
 export function load() {
     return {
         title: derived(t, ($t) => $t.privacy.title),
