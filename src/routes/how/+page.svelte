b1d935e8bacc60cf2323b7b449826ad5465621c1
0nepeop1e <engshun.lim@gmail.com>
2024-12-29 17:43:02 +0800

    complete how to use page

--- src/routes/how/+page.svelte
+++ src/routes/how/+page.svelte
@@ -1,29 +1,118 @@
+<script lang="ts" module>
+    let tab = $state('connect');
+</script>
+
 <script lang="ts">
-    import Localize from '$lib/coms/Localize.svelte';
-    import { t } from '$lib/lang';
+    import Cordova from '$lib/coms/Cordova.svelte';
+    import { Locale, t } from '$lib/lang';
+    import { Tick04Icon, Move02Icon, ViewOffSlashIcon, SearchVisualIcon } from 'hugeicons-svelte';
 
     // TODO: complete this page
 </script>
 
+{#snippet bold(text: string)}
+    <strong class="font-semibold">{text}</strong>
+{/snippet}
+
+{#snippet code(text: string)}
+    <code class="font-mono px-2 py-0.5 bg-gray-4 rounded">{text}</code>
+{/snippet}
+
+{#snippet local_ip(text: string)}
+    <a
+        href="https://support.microsoft.com/en-us/windows/find-your-ip-address-in-windows-f21a9bbc-c582-55cd-35e0-73431160a1b9"
+        target="_blank"
+        class="link link-secondary"
+    >
+        {text}
+    </a>
+{/snippet}
+
+{#snippet scan_button()}
+    <SearchVisualIcon size={20} class="text-purple-10 inline-block mx-0.5" />
+{/snippet}
+
+{#snippet ok_button()}
+    <Tick04Icon size={20} class="text-green-10 inline-block mx-0.5" />
+{/snippet}
+
+{#snippet drag_button()}
+    <Move02Icon size={20} class="text-gray-10 inline-block mx-0.5" />
+{/snippet}
+
+{#snippet hide_button()}
+    <ViewOffSlashIcon size={20} class="text-purple-10 inline-block mx-0.5" />
+{/snippet}
+
 <div class="accordion-group">
-    <button class="accordion outline-none text-justify" tabindex="0">
-        <h2 class="accordion-title">{t.how.connect}</h2>
-        <div class="accordion-content">
+    <div class="accordion outline-none">
+        <input
+            type="radio"
+            bind:group={tab}
+            value="connect"
+            id="how-connect"
+            class="accordion-toggle"
+        />
+        <label for="how-connect" class="accordion-title">{t.how.connect.title}</label>
+        <div class="accordion-content px-4">
             <div class="min-h-0">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla necessitatibus iusto
-                laborum autem placeat aspernatur inventore eius deleniti reprehenderit? Numquam
-                commodi totam mollitia quod
+                <ol class="list-decimal list-outside ml-6">
+                    <li class="my-1.5">{t.how.connect.network}</li>
+                    <li class="my-1.5">
+                        {t.how.connect.endpoint.desciption}
+                        <ul class="list-disc list-outside ml-6 mr-2 my-2.5">
+                            <li class="my-1.5">
+                                {@render Locale.snippets.how.connect.endpoint.manual({
+                                    bold,
+                                    code,
+                                    local_ip
+                                })}
+                            </li>
+                            <li class="my-1.5">
+                                <Cordova>
+                                    {@render Locale.snippets.how.connect.endpoint.scan({
+                                        bold,
+                                        scan_button
+                                    })}
+                                    {#snippet notAvailable()}
+                                        {@render Locale.snippets.how.connect.endpoint.wss({
+                                            code,
+                                            bold
+                                        })}
+                                    {/snippet}
+                                </Cordova>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="my-1.5">{t.how.connect.authenticate}</li>
+                </ol>
             </div>
         </div>
-    </button>
-    <button class="accordion outline-none text-justify" tabindex="0">
-        <h2 class="accordion-title">{t.how.customize}</h2>
-        <div class="accordion-content">
+    </div>
+    <div class="accordion outline-none">
+        <input
+            type="radio"
+            bind:group={tab}
+            value="customize"
+            id="how-customize"
+            class="accordion-toggle"
+        />
+        <label for="how-customize" class="accordion-title">{t.how.customize.title}</label>
+        <div class="accordion-content px-4">
             <div class="min-h-0">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla necessitatibus iusto
-                laborum autem placeat aspernatur inventore eius deleniti reprehenderit? Numquam
-                commodi totam mollitia quod
+                <ul class="list-disc list-outside ml-6">
+                    <li class="my-1.5">{t.how.customize.edit_mode}</li>
+                    <li class="my-1.5">
+                        {@render Locale.snippets.how.customize.drag({ drag_button })}
+                    </li>
+                    <li class="my-1.5">
+                        {@render Locale.snippets.how.customize.hide({ hide_button })}
+                    </li>
+                    <li class="my-1.5">
+                        {@render Locale.snippets.how.customize.done({ ok_button })}
+                    </li>
+                </ul>
             </div>
         </div>
-    </button>
+    </div>
 </div>
--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import { t } from '$lib/lang';
-    import Localize from '$lib/coms/Localize.svelte';
 </script>
 
 <div class="mx-auto max-w-sm min-h-full px-4 text-justify">
@@ -14,7 +13,6 @@
     </ul>
     <h2 class="text-lg font-semibold mt-8 mb-4">
         {t.privacy.data_usage.title}
-        <Localize en="Data Usage" zh_CN="数据使用" zh_TW="數據使用" />
     </h2>
     <ul class="list-disc list-outside ml-8">
         <li class="list-item">
